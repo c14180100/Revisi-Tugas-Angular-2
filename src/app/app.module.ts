2996ad2fb7b1258a653405c3cc0697ee0e7f977e
c14180100 <c14180100@john.petra.ac.id>
2020-05-02 20:52:09 +0700

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { GlobSerService } from './glob-ser.service';
+import { InputPageComponent } from './input-page/input-page.component';
+import { ListPageComponent } from './list-page/list-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, InputPageComponent, ListPageComponent, EditPageComponent ],
   bootstrap:    [ AppComponent ],
   providers: [GlobSerService]
 })
